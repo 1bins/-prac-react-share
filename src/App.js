83d76ad006acb6004b2bf5bf0398c4b26c7603cd
서원빈 <1bins@kakao.com>
2024-04-29 09:23:14 +0900

    :hammer: react-roter-dom 세팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from "./pages/Home";
+import Kakao from "./pages/Kakao";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route path={'/'} element={<Home />}></Route>
+      <Route path={'/kakao'} element={<Kakao />}></Route>
+    </Routes>
   );
 }
 
